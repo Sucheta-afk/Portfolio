4649ee8f7fa43be9c04593611be0d47e8de45dbf
Such-a-genius <145530745+Sucheta-afk@users.noreply.github.com>
2025-01-15 19:46:28 +0530

    Home.js

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
           </h1>
           <div className='home-buttons'>
             <button className='btn btn-hire'>Hire Me</button><t></t>
-            <a className='btn btn-cv' href='https://docs.google.com/document/d/1nArNJtx6r-3ZjNtWB_Hn9xyKwpa7Hs1yXmd17Fvyna8/edit?usp=sharing' target='_blank'  rel="noreferrer">My Resume</a>
+            <a className='btn btn-cv' href='https://docs.google.com/document/d/1NZ-E2O4vkRPdPPUkr1ibwLMup9PQ01_yJRcC8x_XvcQ/edit?usp=sharing' target='_blank'  rel="noreferrer">My Resume</a>
 
           </div>
         </div>
